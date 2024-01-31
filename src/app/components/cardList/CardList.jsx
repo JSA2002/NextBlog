e547aa86ab44cbe82d5203e9ff6fe0c5aa32e52e
JSA2002 <jeetagrawal2002@gmail.com>
2024-01-31 21:21:46 +0530

    dependencies updated

--- src/app/components/cardList/CardList.jsx
+++ src/app/components/cardList/CardList.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import styles from "./cardList.module.css";
+import styles from "../cardList/cardlist.module.css";
 import Pagination from "../pagination/Pagination";
 import Card from "../card/Card";
 
 const getData = async (page, cat) => {
   const res = await fetch(
-    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+    `${process.env.NEXTAUTH_URL}/api/posts?page=${page}&cat=${cat || ""}`,
     {
       cache: "no-store",
     }
